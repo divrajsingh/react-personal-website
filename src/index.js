67d9447b1026d5f77b241cde3578e8c2447628dc
divrajsingh <42528732+divrajsingh@users.noreply.github.com>
2021-09-24 18:47:15 +1000

    api skeleton
    added dotenv package to hide api key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';   
- 
+require("dotenv").config(); 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/Api.js
+++ src/pages/Api.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Flex} from '../components/Flex'
+require("dotenv").config(); 
 
 export function Api() {
-    return (
-        <Flex container="flex">
-            <p>API Page</p>
-        </Flex> 
-    )
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [apiData, setApiData] = useState({}); 
+    const [mediaUrl, setMediaUrl] = useState('');
+
+    useEffect(() => {
+        let API_KEY = process.env.API_KEY;
+        console.log("call made");
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setIsLoaded(true); 
+            setApiData(data);
+            setMediaUrl(data.url);
+        })
+    }, []); 
+
+    if (!isLoaded) {
+        return <div>Loading...</div>; 
+    } else if (mediaUrl.includes("youtube")) {
+        return (
+            <Flex>
+                <h2>NASA API - {apiData.title}</h2>
+                <p>The following media is from NASA's <a href="https://apod.nasa.gov/apod/" rel="noreferrer" target="_blank">Astronomy Picture of the Day</a> service</p>
+                <iframe width="960" height="540" src={mediaUrl}> </iframe>
+            </Flex> 
+        )
+    } else {
+        return (
+            <Flex>
+                <h2>NASA API - {apiData.title}</h2>
+                <p>The following media is from NASA's <a href="https://apod.nasa.gov/apod/" rel="noreferrer" target="_blank">Astronomy Picture of the Day</a> service</p>
+                <img src={mediaUrl}/>
+            </Flex> 
+        )
+    }
+    
 }
 
