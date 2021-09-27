1deb89639772b28108ed0b2e35b187cdbf17f6c2
dsingh310 <divraj.singh@dxc.com>
2021-09-28 09:10:46 +1000

    Transition glitch fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'; 
+import React from 'react';
 import "../style.css"
 
 export function Footer() {
     const date = new Date()
-    return (
-        <div className="footer">
-                <p> <a href="https://github.com/divrajsingh/react-personal-website" rel="noreferrer" target="_blank"> Github</a> | <span class="accent"> {date.getFullYear()} </span></p>
+    return ( <
+        div className = "footer" >
+        <p> < a href = "https://github.com/divrajsingh/react-personal-website" rel = "noreferrer" target = "_blank" > Github </a> | <span className="accent"> {date.getFullYear()} </span > </p> 
         </div>
     )
 }
